b40b07367d6d5027ebd0a6aa76875a0d4883d1c7
tuan-le-dss <minhtuan.le@dssolution.jp>
2024-04-15 11:10:02 +0700

    update code

--- test/test-functions.test.js
+++ test/test-functions.test.js
@@ -6,4 +6,10 @@ describe("test-coverage test suite", () => {
     expect(result.value).toBe(30);
     expect(result.message).toBe(null);
   });
+
+  it("calculateValue test 2", () => {
+    const result = calculateValue(100, 20);
+    expect(result.value).toBe(120);
+    expect(result.message).toBe("Warning: result is greater then 100");
+  });
 });
