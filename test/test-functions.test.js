4ad6fb649d6c29b55e6753bbe21a48d7beff3ca9
tuan-le-dss <minhtuan.le@dssolution.jp>
2024-04-15 10:44:28 +0700

    update code

--- test/test-functions.test.js
+++ test/test-functions.test.js
@@ -3,5 +3,7 @@ const { calculateValue } = require("../test-functions");
 describe("test-coverage test suite", () => {
   it("tests something", () => {
     expect(calculateValue(10, 20).value).toBe(30);
+    expect(result.value).toBe(30);
+    expect(result.message).toBe(null);
   });
 });
